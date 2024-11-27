5ab12f9328c64509d0ea6235057fdc78553b3674
noadoisy <noa.doisy30@gmail.com>
2024-11-27 22:04:45 +0000

    Modif page cours

---
+++ pageC/dev_web/TP2/TPFRM/testFrm.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="author" content="">
+<title>Traitement d'un formulaire en PHP</title>
+</head>
+
+<body>
+
+<h1>Traitement d'un formulaire HTML avec un script en PHP</h1>
+
+<h3>Ce script affiche les données du formulaire :</h3>
+
+<h4>Par la méthode POST</h4><p></p><h4>Par la méthode GET</h4><p></p>
+</body>
+</html>
